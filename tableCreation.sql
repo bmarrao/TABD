2d776e9b4233128ced9429eac1b719d2851216a7
bmarrao <marraob@gmail.com>
2024-05-02 21:10:11 +0100

    new schema sql

--- tableCreation.sql
+++ tableCreation.sql
@@ -1,34 +1,24 @@
-DROP TABLE IF EXISTS agency;
-DROP TABLE IF EXISTS stops;
-DROP TABLE IF EXISTS routes;
-DROP TABLE IF EXISTS calendar;
-DROP TABLE IF EXISTS calendar_dates;
-DROP TABLE IF EXISTS shapes;
-DROP TABLE IF EXISTS stop_times;
-DROP TABLE IF EXISTS transfers;
-DROP TABLE IF EXISTS trips;
-
-CREATE TABLE agency
-(
-  agency_id         TEXT UNIQUE NULL,
-  agency_name       TEXT NOT NULL,
-  agency_url        TEXT NOT NULL,
-  agency_timezone   TEXT NOT NULL,
-  agency_lang       TEXT NULL
-);
+DROP TABLE IF EXISTS transfers CASCADE;
+DROP TABLE IF EXISTS stop_times CASCADE;
+DROP TABLE IF EXISTS trips CASCADE;
+DROP TABLE IF EXISTS calendar_dates CASCADE;
+DROP TABLE IF EXISTS calendar CASCADE;
+DROP TABLE IF EXISTS routes CASCADE;
+DROP TABLE IF EXISTS stops CASCADE;
+DROP TABLE IF EXISTS shapes CASCADE;
 
 CREATE TABLE stops
 (
   stop_id           TEXT PRIMARY KEY,
   stop_code         TEXT NULL,
   stop_name         TEXT NOT NULL,
-  stop_location     POINT NOT NULL, -- Use POINT data type for location
+  stop_location     POINT NOT NULL, 
   zone_id           TEXT NULL,
   stop_url          TEXT NULL
 );
 
 CREATE TABLE routes
-(
+( 
   route_id          TEXT PRIMARY KEY,
   route_short_name  TEXT NULL,
   route_long_name   TEXT NULL,
@@ -49,52 +39,59 @@ CREATE TABLE calendar
   friday            BOOLEAN NOT NULL,
   saturday          BOOLEAN NOT NULL,
   sunday            BOOLEAN NOT NULL,
-  start_date        NUMERIC(8) NOT NULL,
-  end_date          NUMERIC(8) NOT NULL
+  start_date        DATE NOT NULL,
+  end_date          DATE NOT NULL
 );
 
 CREATE TABLE calendar_dates
 (
   service_id        TEXT NOT NULL,
-  date              NUMERIC(8) NOT NULL,
-  exception_type    INTEGER NOT NULL
+  date              DATE NOT NULL,
+  exception_type    INTEGER NOT NULL,
+  PRIMARY KEY (service_id, date),
+  FOREIGN KEY (service_id) REFERENCES calendar(service_id)
 );
 
 CREATE TABLE shapes
 (
-  shape_id          TEXT,
-  shape_pt_location POINT NOT NULL, -- Use POINT data type for location
-  shape_pt_sequence INTEGER NOT NULL
+  shape_id          TEXT PRIMARY KEY,
+  shape_linestring  geometry(LineString)
 );
 
 CREATE TABLE trips
 (
+  trip_id           TEXT PRIMARY KEY,
   route_id          TEXT NOT NULL,
   service_id        TEXT NOT NULL,
-  trip_id           TEXT NOT NULL PRIMARY KEY,
   trip_headsign     TEXT NULL,
   direction_id      BOOLEAN NULL,
   block_id          TEXT NULL,
   shape_id          TEXT NULL,
   wheelchair_accessible TEXT NULL,
   FOREIGN KEY (route_id) REFERENCES routes(route_id),
-  FOREIGN KEY (service_id) REFERENCES calendar(service_id)
+  FOREIGN KEY (service_id) REFERENCES calendar(service_id),
+  FOREIGN KEY (shape_id) REFERENCES shapes(shape_id)
 );
 
 CREATE TABLE stop_times
 (
   trip_id           TEXT NOT NULL,
-  arrival_time      INTERVAL NOT NULL,
-  departure_time    INTERVAL NOT NULL,
+  arrival_time      TIME NOT NULL,
+  departure_time    TIME NOT NULL,
   stop_id           TEXT NOT NULL,
   stop_sequence     INTEGER NOT NULL,
   stop_headsign     TEXT NULL,
-  FOREIGN KEY (trip_id) REFERENCES trips(trip_id)
+  PRIMARY KEY (trip_id, stop_sequence),
+  FOREIGN KEY (trip_id) REFERENCES trips(trip_id),
+  FOREIGN KEY (stop_id) REFERENCES stops(stop_id)
 );
 
 CREATE TABLE transfers
 (
   from_stop_id      TEXT NOT NULL,
   to_stop_id        TEXT NOT NULL,
-  transfer_type     INTEGER NOT NULL
+  transfer_type     INTEGER NOT NULL,
+  PRIMARY KEY (from_stop_id, to_stop_id),
+  FOREIGN KEY (from_stop_id) REFERENCES stops(stop_id),
+  FOREIGN KEY (to_stop_id) REFERENCES stops(stop_id)
 );
--- txt-to-sql.py
+++ txt-to-sql.py
@@ -1,34 +1,57 @@
 import psycopg2
+from datetime import time, datetime, timedelta, date
+
 #psql -U brenin -d postgres
 # Connect to the database
 conn = psycopg2.connect("dbname=postgres user=brenin")
 cursor = conn.cursor()
 
+
+def normalize_time(time_str):
+    hours, minutes, seconds = map(int, time_str.split(':'))
+    if hours >= 24:
+        excess_hours = hours % 24
+        excess_days = hours // 24
+        excess_time = timedelta(hours=excess_hours, minutes=minutes, seconds=seconds)
+        today = date.today()
+        normalized_time = datetime.combine(today, time(0, 0)) + excess_time
+        return normalized_time.time()
+    else:
+        return time(hours, minutes, seconds)
+
+
+
+
+
+
 with open("data/shapes.txt", 'r') as f:
-    # Skip the header line
     next(f)
-    # Read the file line by line
+    shape_id = None
+    shape_points = []
     for line in f:
-        # Split the line into fields
         fields = line.strip().split(",")
-        # Extract values for each field
-        shape_id = fields[0]
+        current_shape_id = fields[0]
         shape_pt_lat = float(fields[1])
         shape_pt_lon = float(fields[2])
-        shape_pt_sequence = int(fields[3])
-        
-        # Insert values into the database
-        cursor.execute("INSERT INTO shapes (shape_id, shape_pt_location, shape_pt_sequence) VALUES (%s, ST_SetSRID(ST_MakePoint(%s, %s), 4326)::POINT, %s)", (shape_id, shape_pt_lon, shape_pt_lat, shape_pt_sequence))
-
+        if current_shape_id != shape_id:
+            if shape_points:
+                linestring = "LINESTRING("
+                linestring += ",".join([f"{lon} {lat}" for lat, lon in shape_points])
+                linestring += ")"
+                cursor.execute("INSERT INTO shapes (shape_id, shape_linestring) VALUES (%s, ST_GeomFromText(%s, 4326))", (shape_id, linestring))
+            shape_id = current_shape_id
+            shape_points = []
+        shape_points.append((shape_pt_lon, shape_pt_lat))
+    if shape_points:
+        linestring = "LINESTRING("
+        linestring += ",".join([f"{lon} {lat}" for lat, lon in shape_points])
+        linestring += ")"
+        cursor.execute("INSERT INTO shapes (shape_id, shape_linestring) VALUES (%s, ST_GeomFromText(%s, 4326))", (shape_id, linestring))
 
 with open("data/routes.txt", 'r') as f:
-    # Skip the header line
     next(f)
-    # Read the file line by line
     for line in f:
-        # Split the line into fields
         fields = line.strip().split(",")
-        # Extract values for each field
         route_id = fields[0]
         route_short_name = fields[1]
         route_long_name = fields[2]
@@ -36,18 +59,12 @@ with open("data/routes.txt", 'r') as f:
         route_url = fields[5]
         route_color = fields[6]
         route_text_color = fields[7]
-        
-        # Insert values into the database
         cursor.execute("INSERT INTO routes (route_id, route_short_name, route_long_name, route_type, route_url, route_color, route_text_color) VALUES (%s, %s, %s, %s, %s, %s, %s)", (route_id, route_short_name, route_long_name, route_type, route_url, route_color, route_text_color))
 
 with open("data/stops.txt", 'r') as f:
-    # Skip the header line
     next(f)
-    # Read the file line by line
     for line in f:
-        # Split the line into fields
         fields = line.strip().split(",")
-        # Extract values for each field
         stop_id = fields[0]
         stop_code = fields[1]
         stop_name = fields[2]
@@ -55,18 +72,22 @@ with open("data/stops.txt", 'r') as f:
         stop_lon = float(fields[4])
         zone_id = fields[5]
         stop_url = fields[6]
-        
-        # Insert values into the database
         cursor.execute("INSERT INTO stops (stop_id, stop_code, stop_name, stop_location, zone_id, stop_url) VALUES (%s, %s, %s, ST_SetSRID(ST_MakePoint(%s, %s), 4326)::POINT, %s, %s)", (stop_id, stop_code, stop_name, stop_lon, stop_lat, zone_id, stop_url))
 
-with open("data/calendar.txt", 'r') as f:
-    # Skip the header line
+with open("data/transfers.txt", 'r') as f:
+
     next(f)
-    # Read the file line by line
     for line in f:
-        # Split the line into fields
         fields = line.strip().split(",")
-        # Extract values for each field
+        from_stop_id = fields[0]
+        to_stop_id = fields[1]
+        transfer_type = int(fields[2])
+
+        cursor.execute("INSERT INTO transfers (from_stop_id, to_stop_id, transfer_type) VALUES (%s, %s, %s)", (from_stop_id, to_stop_id, transfer_type))
+with open("data/calendar.txt", 'r') as f:
+    next(f)
+    for line in f:
+        fields = line.strip().split(",")
         service_id = fields[0]
         monday = fields[1] == "1"
         tuesday = fields[2] == "1"
@@ -75,84 +96,71 @@ with open("data/calendar.txt", 'r') as f:
         friday = fields[5] == "1"
         saturday = fields[6] == "1"
         sunday = fields[7] == "1"
-        start_date = int(fields[8])
-        end_date = int(fields[9])
-        
-        # Insert values into the database
+        start_date_str = fields[8]
+        end_date_str = fields[9]
+        start_date = datetime.strptime(start_date_str, "%Y%m%d").date()
+        end_date = datetime.strptime(end_date_str, "%Y%m%d").date()
         cursor.execute("INSERT INTO calendar (service_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday, start_date, end_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (service_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday, start_date, end_date))
 
 with open("data/calendar_dates.txt", 'r') as f:
-    # Skip the header line
     next(f)
-    # Read the file line by line
     for line in f:
-        # Split the line into fields
         fields = line.strip().split(",")
-        # Extract values for each field
         service_id = fields[0]
-        date = int(fields[1])
+        date_str = fields[1]
         exception_type = int(fields[2])
-        
-        # Insert values into the database
+        date = datetime.strptime(date_str, "%Y%m%d").date()
         cursor.execute("INSERT INTO calendar_dates (service_id, date, exception_type) VALUES (%s, %s, %s)", (service_id, date, exception_type))
 
-
-with open("data/transfers.txt", 'r') as f:
-    # Skip the header line
-    next(f)
-    # Read the file line by line
-    for line in f:
-        # Split the line into fields
-        fields = line.strip().split(",")
-        # Extract values for each field
-        from_stop_id = fields[0]
-        to_stop_id = fields[1]
-        print(fields[2])
-        transfer_type = int(fields[2])
-
-        # Insert values into the database
-        cursor.execute("INSERT INTO transfers (from_stop_id, to_stop_id, transfer_type) VALUES (%s, %s, %s)", (from_stop_id, to_stop_id, transfer_type))
-        
-with open("data/trips.txt", 'r') as f:
-    # Skip the header line
-    next(f)
-    # Read the file line by line
-    for line in f:
-        # Split the line into fields
-        route_id, direction_id, service_id, trip_id, trip_headsign, wheelchair_accessible, block_id, shape_id = line.strip().split(",")
-        
-        # Convert direction_id to boolean
-        direction_id = bool(int(direction_id))
-        
-        # Convert wheelchair_accessible to boolean
-        wheelchair_accessible = bool(int(wheelchair_accessible))
-        
-        # Check for empty strings and convert to None
-        block_id = block_id if block_id else None
-        shape_id = shape_id if shape_id else None
-        
-        # Insert values into the database
-        cursor.execute("INSERT INTO trips (route_id, direction_id, service_id, trip_id, trip_headsign, wheelchair_accessible, block_id, shape_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (route_id, direction_id, service_id, trip_id, trip_headsign, wheelchair_accessible, block_id, shape_id))
-
 with open("data/stop_times.txt", 'r') as f:
-    # Skip the header line
     next(f)
-    # Read the file line by line
     for line in f:
-        # Split the line into fields
         fields = line.strip().split(",")
-        # Extract values for each field
         trip_id = fields[0]
-        arrival_time = fields[1]
-        departure_time = fields[2]
+        arrival_time_str = fields[1]
+        departure_time_str = fields[2]
         stop_id = fields[3]
         stop_sequence = int(fields[4])
         stop_headsign = fields[5] if len(fields) > 5 else None
+        arrival_time = normalize_time(arrival_time_str)
+        departure_time = normalize_time(departure_time_str)
+        cursor.execute("INSERT INTO stop_times (trip_id, arrival_time, departure_time, stop_id, stop_sequence, stop_headsign) VALUES (%s, %s, %s, %s, %s, %s)", (trip_id, arrival_time, departure_time, stop_id, stop_sequence, stop_headsign))
+
+with open("data/trips.txt", 'r') as f:
+
+    next(f)
+
+    for line in f:
+        route_id, direction_id, service_id, trip_id, trip_headsign, wheelchair_accessible, block_id, shape_id = line.strip().split(",")
+        
+        direction_id = bool(int(direction_id))
+        
+        wheelchair_accessible = bool(int(wheelchair_accessible))
+        
+        block_id = block_id if block_id else None
+        shape_id = shape_id if shape_id else None
+        
+        cursor.execute("INSERT INTO trips (route_id, direction_id, service_id, trip_id, trip_headsign, wheelchair_accessible, block_id, shape_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (route_id, direction_id, service_id, trip_id, trip_headsign, wheelchair_accessible, block_id, shape_id))
+
+with open("data/stop_times.txt", 'r') as f:
+    next(f)
+    for line in f:
+        fields = line.strip().split(",")
+        # Extract values for each field
+        trip_id = fields[0]
+        arrival_time_str = fields[1]
+        departure_time_str = fields[2]
+        stop_id = fields[3]
+        stop_sequence = int(fields[4])
+        stop_headsign = fields[5] if len(fields) > 5 else None
+        
+        # Normalize time values
+        arrival_time = normalize_time(arrival_time_str)
+        departure_time = normalize_time(departure_time_str)
         
         # Insert values into the database
         cursor.execute("INSERT INTO stop_times (trip_id, arrival_time, departure_time, stop_id, stop_sequence, stop_headsign) VALUES (%s, %s, %s, %s, %s, %s)", (trip_id, arrival_time, departure_time, stop_id, stop_sequence, stop_headsign))
 
 
-# Commit changes and close connection
 conn.commit()
 conn.close()
