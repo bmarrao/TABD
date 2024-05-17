07e70d4c17e14cc1fd513061b9fd737ecfa3df6f
bmarrao <marraob@gmail.com>
2024-05-17 17:09:40 +0100

    adding examples

--- dataWarehouse.sql
+++ dataWarehouse.sql
@@ -1,25 +1,35 @@
-CREATE TABLE dw_time (
+DROP TABLE IF EXISTS dw_time CASCADE;
+DROP TABLE IF EXISTS dw_taxi CASCADE;
+DROP TABLE IF EXISTS dw_location CASCADE;
+DROP TABLE IF EXISTS dw_facts CASCADE;
+
+CREATE TABLE dw_time 
+(
     time_id SERIAL PRIMARY KEY,
     hour INTEGER,
     date DATE,
     month INTEGER
 );
 
-CREATE TABLE dw_taxi (
+CREATE TABLE dw_taxi 
+(
     taxi_id CHARACTER(8) PRIMARY KEY
 );
 
-CREATE TABLE dw_location (
-    location_id SERIAL PRIMARY KEY,
-    taxi_stand INTEGER,
+CREATE TABLE dw_location
+(
+    stop_id SERIAL PRIMARY KEY,
+    location     geometry(Point),
     freguesia character varying(255),
     concelho character varying(255)
 );
 
+
 CREATE TABLE dw_facts (
     time_id INTEGER REFERENCES dw_time(time_id),
     taxi_id CHARACTER(8) REFERENCES dw_taxi(taxi_id),
-    location_id INTEGER REFERENCES dw_location(location_id),
+    initial_stop INTEGER REFERENCES dw_location(initial_stop),
+    final_stop INTEGER REFERENCES dw_location(final_stop),
     number_of_trips INTEGER,
     PRIMARY KEY (time_id, taxi_id, location_id)
 );
